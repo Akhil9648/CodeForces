84522e88496c4383ed6acc5e0a8093ad309d364c
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-03 20:20:06 +0530

    Create A - Cloudberry Jam.cpp

---
+++ Cpp/A - Cloudberry Jam.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k;
+    cin>>k;
+    while(k--)
+    {
+        int n;
+        cin>>n;
+        cout<<n*2<<"\n";
+    }
+    return 0;
+}
