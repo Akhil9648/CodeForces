7a4923f8b0d82003b54542ea95c1ed0abbb2fb2e
Akhil Pandey <akhilpandey494@gmail.com>
2025-04-12 14:19:12 +0530

    Create 1335A - Candies and Two Sisters.cpp

---
+++ Cpp/1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a;
+        cin>>a;
+        a--;
+        cout<<a/2<<"\n";
+    }
+    return 0;
+}
