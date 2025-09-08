027f32ee56299980232d1c378bedaaa149743157
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-09 00:39:48 +0530

    Create A_Maple_and_Multiplication.cpp

---
+++ Cpp/A_Maple_and_Multiplication.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k;
+    cin>>k;
+    while(k--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a==b) cout<<"0\n"; 
+        else if(a%b==0 || b%a==0) cout<<"1\n";
+        else cout<<"2\n";
+    }
+    return 0;
+}
