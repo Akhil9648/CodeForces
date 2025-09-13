0bf4829af1a6f9a025f092605ae2c2d135406a80
Akhil Pandey <akhilpandey494@gmail.com>
2025-09-13 19:10:14 +0530

    Create A_Collatz_Conjecture.cpp

---
+++ Cpp/A_Collatz_Conjecture.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k;
+    cin>>k;
+    while(k--)
+    {
+        long long a,b;
+        cin>>a>>b;
+        long long ans=b<<a;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
